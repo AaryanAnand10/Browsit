e16f36a28715853eaddb00d0113b337bc9460f94
Hrishi <70897209+hrishi-sarma@users.noreply.github.com>
2024-07-28 19:30:43 +0530

    Update Browsit.py

--- Browsit.py
+++ Browsit.py
@@ -1,11 +1,22 @@
+import os
 import sys
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 from PyQt6.QtCore import *
 from PyQt6.QtWebEngineWidgets import *
 
+class CustomWebEnginePage(QWebEngineView):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def acceptNavigationRequest(self, url, type, isMainFrame):
+        if type == QWebEngineView.NavigationType.NavigationTypeLinkClicked:
+            self.setUrl(url)
+            return False
+        return super().acceptNavigationRequest(url, type, isMainFrame)
+
 class HoverSidebar(QWidget):
-    def __init__(self, parent, width, extended_width):
+    def __init__(self, parent, width, extended_width):  
         super().__init__(parent)
         self.normal_width = width
         self.extended_width = extended_width
@@ -40,6 +51,7 @@ class HoverSidebar(QWidget):
     def set_normal_width(self, width):
         self.normal_width = width
         self.setFixedWidth(self.normal_width)
+        
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -67,6 +79,11 @@ class MainWindow(QMainWindow):
 
         navbar.addWidget(self.url_bar)
 
+        # Add a button to open the chatbot
+        chatbot_btn = QPushButton("Chatbot")
+        chatbot_btn.clicked.connect(self.open_chatbot)
+        navbar.addWidget(chatbot_btn)
+
         # Set the main window properties
         self.setWindowTitle("Browsit")
         self.setGeometry(100, 100, 1920, 1080)
@@ -78,24 +95,7 @@ class MainWindow(QMainWindow):
             }
         """)  # Change background to the image
         self.showMaximized()
-
-        # Create shortcuts for navigating back and forward
-        self.shortcutPrevPage = QShortcut(QKeySequence("Alt+Left"), self)
-        self.shortcutPrevPage.activated.connect(self.navigate_back)
-
-        self.shortcutForwPage = QShortcut(QKeySequence("Alt+Right"), self)
-        self.shortcutForwPage.activated.connect(self.navigate_forward)
-
-        self.shortcutUrlFocus = QShortcut(QKeySequence("Ctrl+/"), self)
-        self.shortcutUrlFocus.activated.connect(self.focus_url_bar)
-
-        self.shortcutNewTab = QShortcut(QKeySequence("Ctrl+T"), self)
-        self.shortcutNewTab.activated.connect(self.add_new_tab)
-
-        # Create the Ctrl + W shortcut for closing the current tab
-        self.shortcutCloseTab = QShortcut(QKeySequence("Ctrl+W"), self)
-        self.shortcutCloseTab.activated.connect(self.close_current_tab)
-
+    
         # Create the central widget and set layout
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
@@ -103,7 +103,7 @@ class MainWindow(QMainWindow):
         self.initUI()
 
         # Connect returnPressed signals
-        self.text_input.returnPressed.connect(self.perform_search)
+        self.text_input.returnPressed.connect(self.perform_search_google)
 
     def initUI(self):
         # Create the main layout
@@ -115,7 +115,7 @@ class MainWindow(QMainWindow):
         
 
         # Create the Browsit label
-        browsit_label = QLabel("Browsit...", self)
+        browsit_label = QLabel("Browsit", self)
         browsit_label.setFont(QFont("Courier", 25))  # Smaller font size
         browsit_label.setStyleSheet("color: #FFFFFF;")  # White text color
         browsit_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -137,13 +137,6 @@ class MainWindow(QMainWindow):
         self.text_input.setFont(QFont("Courier", 12, QFont.Weight.Bold))  # Smaller font size
         input_layout.addWidget(self.text_input)
         
-        self.go_button = QPushButton("GO", input_container)
-        self.go_button.setFont(QFont("Courier", 7, QFont.Weight.Bold, italic=True))  # Smaller font size
-        self.go_button.setStyleSheet("color: #FFFFFF;")  # White text color
-        self.go_button.clicked.connect(self.perform_search)
-        input_layout.addWidget(self.go_button)
-
-        # main_layout.addWidget(input_container, alignment=Qt.AlignmentFlag.AlignCenter)
         self.brows_layout.addWidget(input_container, alignment=Qt.AlignmentFlag.AlignCenter)
 
         self.brows_layout.setContentsMargins(0, 20, 0, 0)
@@ -156,43 +149,35 @@ class MainWindow(QMainWindow):
 
         # Add the first empty tab with a default web view
         self.add_new_tab()
-        
-        # Add buttons to the icons container layout
-        # # Create round button B
-        # round_button2 = QPushButton("B", self)
-        # round_button2.setGeometry(1640, 30, 30, 30)  # Place on the leftmost side, same height as round_button1
-        # round_button2.setFont(QFont("Courier", 8, QFont.Weight.Bold, italic=True))  # Smaller font size
-        # round_button2.setStyleSheet("color: #FFFFFF;")  # White text color
 
         # Create the Chrome button
         self.chrome_button = QPushButton(self)
-        self.chrome_button.setIcon(QIcon("HackathonPrep/google.jpeg"))
+        self.chrome_button.setIcon(QIcon("google.jpeg"))
         self.chrome_button.setIconSize(QSize(30, 30))  # Set the icon size to match the button size
-        self.chrome_button.setGeometry(1500, 67, 30, 30)  # Align horizontally below round_button2
-        self.chrome_button.setCheckable(True)
-        self.chrome_button.setFixedSize(40, 40)  # Same size as round_button2
+        self.chrome_button.setGeometry(1500, 70, 30, 30)  # Align horizontally below round_button2
+        self.chrome_button.clicked.connect(self.perform_search_google)
+        self.chrome_button.setFixedSize(30, 30)  # Same size as round_button2
         self.chrome_button.setStyleSheet("background-color: none;")
 
         # Create the Duck button
         self.duck_button = QPushButton(self)
-        self.duck_button.setIcon(QIcon("HackathonPrep/ddgico.jpg"))
+        self.duck_button.setIcon(QIcon("ddgico.jpg"))
         self.duck_button.setIconSize(QSize(30, 30))  # Set the icon size to match the button size
-        self.duck_button.setGeometry(1620, 67, 30, 30)  # Align horizontally below round_button2
-        self.duck_button.setCheckable(True)
-        self.duck_button.setFixedSize(40, 40)  # Same size as round_button2
+        self.duck_button.setGeometry(1620, 70, 30, 30)  # Align horizontally below round_button2
+        self.duck_button.clicked.connect(self.perform_search_duck)
+        self.duck_button.setFixedSize(30, 30)  # Same size as round_button2
         self.duck_button.setStyleSheet("background-color: none;")
         
         # Create the Yahoo button
         self.yahoo_button = QPushButton(self)
-        self.yahoo_button.setIcon(QIcon("HackathonPrep/yahoo.jpeg"))
+        self.yahoo_button.setIcon(QIcon("yahoo.jpeg"))
         self.yahoo_button.setIconSize(QSize(30, 30))  # Set the icon size to match the button size
-        self.yahoo_button.setGeometry(1560, 67, 30, 30)  # Align horizontally below round_button2
-        self.yahoo_button.setCheckable(True)
-        self.yahoo_button.setFixedSize(40, 40)  # Same size as round_button2
+        self.yahoo_button.setGeometry(1560, 70, 30, 30)  # Align horizontally below round_button2
+        self.yahoo_button.clicked.connect(self.perform_search_yahoo)
+        self.yahoo_button.setFixedSize(30, 30)  # Same size as round_button2
         self.yahoo_button.setStyleSheet("background-color: none;")
 
         # Show the buttons
-        # round_button2.show()
         self.chrome_button.show()
         self.yahoo_button.show()
         self.duck_button.show()
@@ -209,57 +194,73 @@ class MainWindow(QMainWindow):
         if current_index != -1:
             self.tab_widget.removeTab(current_index)
 
-
     def add_new_tab(self, url=None):
         web_view = QWebEngineView(self)
         if url:
             web_view.setUrl(QUrl(url))
         else:
-            web_view.setUrl(QUrl(""))
-        self.tab_widget.addTab(web_view, "New Tab")
+            file_path = os.path.abspath(os.path.join(os.path.dirname(__file__),  "index.html"))
+            local_url = QUrl.fromLocalFile(file_path)
+            web_view.setUrl(local_url)
+        if(url!=None):
+            self.tab_widget.addTab(web_view, f"{url}")
+        else:
+            self.tab_widget.addTab(web_view, 'Home Page')
         self.tab_widget.setCurrentWidget(web_view)
 
-    def perform_search(self):
+    def reload_tab(self):
+        current_tab = self.tab_widget.currentWidget()
+        if isinstance(current_tab, QWebEngineView):
+            current_tab.reload()
+
+    def perform_search_google(self):
         query = self.text_input.text() or self.url_bar.text()
         if not query:
             return
-        search_url1 = f"https://www.google.com/search?q={query}"
-        search_url2 = f"https://search.yahoo.com/search?p={query}"
-        search_url3 = f"https://duckduckgo.com/?q={query}"
+        search_url = f"https://www.google.com/search?q={query}"
+        self.navigate_to_url(QUrl(search_url))
 
-        if self.chrome_button.isChecked():
-            self.add_new_tab(search_url1)
-        elif self.yahoo_button.isChecked():
-            self.add_new_tab(search_url2)   
-        elif self.duck_button.isChecked():
-            self.add_new_tab(search_url3)
-        else:
-            self.add_new_tab(search_url1)
-        
-    def navigate_back(self):
-        current_tab = self.tab_widget.currentWidget()
-        if isinstance(current_tab, QWebEngineView):
-            current_tab.back()
+    def perform_search_yahoo(self):
+        query = self.text_input.text() or self.url_bar.text()
+        if not query:
+            return
+        search_url = f"https://search.yahoo.com/search?p={query}"
+        self.navigate_to_url(QUrl(search_url))
 
-    def navigate_forward(self):
-        current_tab = self.tab_widget.currentWidget()
-        if isinstance(current_tab, QWebEngineView):
-            current_tab.forward()
+    def perform_search_duck(self):
+        query = self.text_input.text() or self.url_bar.text()
+        if not query:
+            return
+        search_url = f"https://duckduckgo.com/?q={query}"
+        self.navigate_to_url(QUrl(search_url))
 
     def navigate_home(self):
-        self.add_new_tab("")
+    # Use the add_new_tab method to open the custom home page
+        file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "index.html"))
+        local_url = QUrl.fromLocalFile(file_path)
+        self.tab_widget.currentWidget().setUrl(local_url)
+        self.tab_widget.setTabText(self.tab_widget.currentIndex(), "Home Page")
 
-    def navigate_to_url(self):
-        url = self.url_bar.text()
-        if not url.startswith("https://"):
-            url = "https://" + url
-        self.add_new_tab(url)
+    def navigate_to_url(self, url=None):
+        if not url:
+            url = self.url_bar.text()
+            if not url.startswith("https://"):
+                url = "https://" + url
+            url = QUrl(url)
+        
+        current_tab = self.tab_widget.currentWidget()
+        if isinstance(current_tab, QWebEngineView):
+            current_tab.setUrl(url)
 
-    def focus_url_bar(self):
-        self.url_bar.setFocus()
+    def open_chatbot(self):
+        chatbot_url = "https://6bd5b0f458be7729a5.gradio.live"
+        self.add_new_tab(chatbot_url)
 
 if __name__ == "__main__":
+    sys.argv.append("--disable-web-security")
+
     app = QApplication(sys.argv)
     mainWin = MainWindow()
     mainWin.show()
     sys.exit(app.exec())
+
